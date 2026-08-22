b40b1b1a6dea4e6998206fcc49d0b9ff777a02d3
cm5229@srmist.edu.in <cm5229@srmist.edu.in>
2026-08-22 22:25:22 +0530

    Add another session 1 Java program

---
+++ src/main/java/string/class_problems/NaturalNumberSum.java
@@ -0,0 +1,20 @@
+package string.class_problems;
+public class NaturalNumberSum {
+
+    public static void sumOfNaturalNumbers(int n) {
+        int sum = 0;
+        int counter = 1;
+
+        while (counter <= n) {
+            sum += counter;
+            counter++;
+        }
+
+        System.out.println("Sum of numbers from 1 to " + n + " = " + sum);
+    }
+
+    public static void main(String[] args) {
+        // Test case from sample output
+        sumOfNaturalNumbers(5);
+    }
+}
